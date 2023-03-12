3b700b7ef919293faa19f4ba4025382f4c7d4274
ALLEN PARTAIN-RAAB <119446724+AllenPartain@users.noreply.github.com>
2023-03-12 23:34:03 +0000

    links working now

--- functions.php
+++ functions.php
@@ -61,9 +61,9 @@ $nav =
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Tasks</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Search</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="/index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/tasks/index.php">Tasks</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/search.php">Search</a></li>
                     </ul>
                 </div>
             </div>
